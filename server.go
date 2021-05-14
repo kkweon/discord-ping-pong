2496256465ddd24f509e3be539d420b8909c9d37
Mo Kweon <kkweon@google.com>
2021-05-13 21:44:41 -0700

    [WIP] INTERACTIONS ENDPOINT URL
    - Running into an error saying
    
      ```
      Validation errors:
    
      interactions_endpoint_url: The specified interactions endpoint url could not be verified.
      ```
    
    - It's something to do with https://discord.com/developers/docs/interactions/slash-commands#security-and-authorization

--- server.go
+++ server.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"crypto/ed25519"
+	"encoding/hex"
 	"net/http"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
+
+var rawKey = []byte("APPLICATION_PUBLIC_KEY")
+
+var applicaitonPublicKey []byte
+
+func init() {
+	applicaitonPublicKey = make([]byte, hex.EncodedLen(len(rawKey)))
+	hex.Encode(applicaitonPublicKey, rawKey)
+}
 
 func main() {
 	r := gin.Default()
@@ -16,12 +27,19 @@ func main() {
 		})
 	})
 	r.POST("/api/interactions", func(c *gin.Context) {
+		if !discordgo.VerifyInteraction(c.Request, ed25519.PublicKey(applicaitonPublicKey)) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
 		var rootMessage discordgo.Message
 		err := c.BindJSON(&rootMessage)
 		logrus.WithError(err).Infof("%+v", rootMessage)
 		if err == nil {
 			if rootMessage.Type == 1 {
-				c.JSON(http.StatusOK, rootMessage)
+				c.JSON(http.StatusOK, gin.H{
+					"type": 1,
+				})
 				return
 			}
 		}
