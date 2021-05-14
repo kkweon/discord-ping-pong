2a209f22a08936ce8dc9572debfd98e79ee1eb03
Mo Kweon <kkweon@google.com>
2021-05-14 16:53:26 -0700

    Complete verification stage

--- server.go
+++ server.go
@@ -4,22 +4,25 @@ import (
 	"crypto/ed25519"
 	"encoding/hex"
 	"net/http"
+	"os"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
-var rawKey = []byte("APPLICATION_PUBLIC_KEY")
-
-var applicaitonPublicKey []byte
-
-func init() {
-	applicaitonPublicKey = make([]byte, hex.EncodedLen(len(rawKey)))
-	hex.Encode(applicaitonPublicKey, rawKey)
-}
-
 func main() {
+	pKey := os.Getenv("APPLICATION_PUBLIC_KEY")
+
+	if pKey == "" {
+		logrus.Fatal("env APPLICATION_PUBLIC_KEY is required. Get your application key from https://discord.com/developers/applications.")
+		return
+	}
+
+	rawKey := []byte(pKey)
+	applicaitonPublicKey := make([]byte, hex.DecodedLen(len(rawKey)))
+	hex.Decode(applicaitonPublicKey, rawKey)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
